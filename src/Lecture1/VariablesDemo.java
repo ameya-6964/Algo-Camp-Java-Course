10d2608010b507221fe97ed8f722bbd53362b898
ameya-6964 <belvalkar7@gmail.com>
2024-02-11 15:27:19 +0530

    Added Variables Code

---
+++ src/Lecture1/VariablesDemo.java
@@ -0,0 +1,24 @@
+package Lecture1;
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+        //Assigning value during declaration.
+        int speedLimit = 80;
+
+        //declaring a variable
+
+        int speed;
+        speed = 20;
+        System.out.println(speed);
+        speed = 50;
+        System.out.println(speed);
+
+        //assigning value to variable
+        int age = 20;
+        int AGE = 25;
+
+        System.out.println(age);
+        System.out.println(AGE);
+        System.out.println(speedLimit);
+    }
+}
