917c3cbb4b7a9241ce456ec001119debaa7d8f9b
ameya-6964 <belvalkar7@gmail.com>
2024-02-11 15:25:07 +0530

    First Commit

---
+++ src/Lecture1/HelloWorld.java
@@ -0,0 +1,19 @@
+package Lecture1;
+
+// "Hello World" program example
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("1. println");
+        System.out.println("2. println");
+
+        System.out.print("1. print ");
+        System.out.print("2. print ");
+
+        System.out.println();
+
+        //System.out.printf(format, arguments);
+        System.out.printf("Hello  %s!%n", "World");
+        System.out.printf("Java is %d percent fun!", 100);
+    }
+}
